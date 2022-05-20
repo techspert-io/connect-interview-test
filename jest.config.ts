314137f553b21e508092442e3530b3c5f10d0021
Stuart Tottle <stuarttottle@hotmail.com>
2022-05-20 17:25:48 +0100

    created app

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["node_modules/(?!variables/.*)"],
+};
